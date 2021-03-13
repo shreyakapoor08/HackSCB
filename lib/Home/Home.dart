5c7177d04621ed97b43d3e9438c1fa1485151cb2
Mishthirao <48217335+Mishthirao@users.noreply.github.com>
2021-03-13 14:34:52 +0530

    Update Home.dart
    Home page is commited by - Manika

--- lib/Home/Home.dart
+++ lib/Home/Home.dart
@@ -1,2 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:women_safety_app/size_conifg.dart';
 
-demo
+import '../size_config.dart';
+
+
+class Home extends StatefulWidget {
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      appBar: AppBar(title: Text('Home'),),
+
+      //drawer to be added here
+
+    );
+  }
+
+}
