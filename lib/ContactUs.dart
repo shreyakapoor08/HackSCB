518ddebc399f57c78239e4ad124d365fca7b5990
Manika Yadav <48217335+Manika110@users.noreply.github.com>
2021-03-14 17:56:24 +0530

    ContactUs is Upadted now - Manika

--- lib/ContactUs.dart
+++ lib/ContactUs.dart
@@ -1,1 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:sms/sms.dart';
 
+class ContactUs extends StatefulWidget {
+  createState()
+  {
+    return ContactUsState();
+  }
+}
+
+
+class ContactUsState extends State<ContactUs> {
+
+  TextStyle textStyle=TextStyle(color: Colors.black);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text('Contact us'),),
+
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            children: [
+              Text(
+                'WeSafe',
+                style: TextStyle(
+                  color: Colors.pink,
+                  fontSize: 30,
+                ),
+              ),
+
+              SizedBox(height: 20,),
+              Text('Call us - 9123XXXXXX \n'
+                  'Mail Us Your Query at- WeSafe@gmail.com',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20
+                ),
+              )
+            ],
+          ),
+
+        ),
+      ),
+    );
+  }
+
+}
